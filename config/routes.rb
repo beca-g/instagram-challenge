9ec7d01ac085af7c06635ef9f4d2795ce0a014a8
Beca-g <rebekahgalliano@gmail.com>
2020-11-22 21:45:49 +0000

    Generated post model, set up active storage.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   
 end
